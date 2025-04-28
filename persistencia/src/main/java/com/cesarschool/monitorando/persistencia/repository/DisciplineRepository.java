5a8bab7eb48148534ad48cae8d575d3eb01594f4
SofiaValadares <113111708+SofiaValadares@users.noreply.github.com>
2025-04-28 14:10:15 -0300

    Delete persistencia directory

--- persistencia/src/main/java/com/cesarschool/monitorando/persistencia/repository/DisciplineRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cesarschool.monitorando.persistencia.repository;
-
-import com.cesarschool.monitorando.dominio.entity.DisciplineEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisciplineRepository extends JpaRepository<DisciplineEntity, Long> {
-
-}
--- persistencia/src/main/java/com/cesarschool/monitorando/persistencia/repository/MonitorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cesarschool.monitorando.persistencia.repository;
-
-import com.cesarschool.monitorando.dominio.entity.MonitorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonitorRepository extends JpaRepository<MonitorEntity, Long> {}
