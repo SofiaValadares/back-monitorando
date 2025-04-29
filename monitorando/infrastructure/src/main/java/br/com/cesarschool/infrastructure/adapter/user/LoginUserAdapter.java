940c4610d0dcfe8d8eae66d9211d9cacfbb582bf
SofiaValadares <svc2@cesar.school>
2025-04-29 15:38:13 -0300

    start login

--- monitorando/infrastructure/src/main/java/br/com/cesarschool/infrastructure/adapter/user/LoginUserAdapter.java
+++ monitorando/infrastructure/src/main/java/br/com/cesarschool/infrastructure/adapter/user/LoginUserAdapter.java
@@ -2,6 +2,7 @@ package br.com.cesarschool.infrastructure.adapter.user;
 
 import br.com.cesarschool.application.port.user.LoginUserPort;
 import br.com.cesarschool.domain.entity.UserEntity;
+import br.com.cesarschool.domain.exception.LoginIncorrectException;
 import br.com.cesarschool.infrastructure.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,10 +16,10 @@ public class LoginUserAdapter implements LoginUserPort<UserEntity> {
     @Override
     public UserEntity loginUser(String email, String password) {
         UserEntity user = userRepository.findByEmail(email)
-            .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado com o e-mail: " + email));
+            .orElseThrow(LoginIncorrectException::new);
 
         if (!user.getPassword().equals(password)) {
-            throw new IllegalArgumentException("Senha incorreta.");
+            throw new LoginIncorrectException();
         }
 
         return user;
--- monitorando/presentation/src/main/java/br/com/cesarschool/presentation/handler/GlobalExceptionHandler.java
+++ monitorando/presentation/src/main/java/br/com/cesarschool/presentation/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.cesarschool.presentation.handler;
 
 import br.com.cesarschool.domain.exception.EmailAlreadyInUseException;
+import br.com.cesarschool.domain.exception.LoginIncorrectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,11 +16,20 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyInUseException.class)
     public ResponseEntity<Map<String, Object>> handleEmailAlreadyInUse(EmailAlreadyInUseException ex) {
+        return getMapResponseEntity(ex.getMessage(), ex);
+    }
+
+    @ExceptionHandler(LoginIncorrectException.class)
+    public ResponseEntity<Map<String, Object>> handleEmailAlreadyInUse(LoginIncorrectException ex) {
+        return getMapResponseEntity(ex.getMessage(), ex);
+    }
+
+    private ResponseEntity<Map<String, Object>> getMapResponseEntity(String message, RuntimeException ex) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpStatus.CONFLICT.value());
         body.put("error", "Conflict");
-        body.put("message", ex.getMessage());
+        body.put("message", message);
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(body);
     }
