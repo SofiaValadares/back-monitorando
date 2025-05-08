476562479af55dfa080fcc9e7f5cb6502097fe3e
aaon <aaon@gmail.com>
2025-05-07 21:59:05 -0300

    Criação do AvailableTimeMapper

--- monitorando/domain/src/main/java/br/com/cesarschool/domain/entity/AvailableTimeEntity.java
+++ monitorando/domain/src/main/java/br/com/cesarschool/domain/entity/AvailableTimeEntity.java
@@ -5,16 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.management.monitor.Monitor;
 import java.time.LocalTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MonitorAvailabilityEntity {
-    private WeekDay weekDay;
+public class AvailableTimeEntity {
+
+    private Long id; // <-- compatível com @Id do JPA
+    private WeekDay weekDay; // precisa garantir compatibilidade no JPA
     private LocalTime startTime;
     private LocalTime endTime;
-    private Monitor monitor;
-
 }
